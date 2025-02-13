725c6d51e49e048679bad27d416df47b6a074d05
Anastasia <aachuchkalova@gmail.com>
2025-02-13 14:10:42 +0700

    feat:two functions have been created to convert rgb to hex and vice versa

--- src/colorConverter.jsx
+++ src/colorConverter.jsx
@@ -1,24 +1,52 @@
 import { useState } from "react";
-import "./colorConverter.css"; // Подключаем стили
+import "./ColorConverter.css";
 
 function ColorConverter() {
+  const [rgb, setRgb] = useState(["", "", ""]);
+  const [hex, setHex] = useState("");
+
+  const rgbToHex = (r, g, b) => {
+    if (r > 255 || g > 255 || b > 255) return "";
+    return `#${r.toString(16).padStart(2, "0")}${g
+      .toString(16)
+      .padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
+  };
+
+  const hexToRgb = (hex) => {
+    if (hex.length !== 6) return "";
+    const r = parseInt(hex.slice(0, 2), 16);
+    const g = parseInt(hex.slice(2, 4), 16);
+    const b = parseInt(hex.slice(4, 6), 16);
+    return [r, g, b];
+  };
+
+  const handleRgbChange = () => {};
+
+  const handleHexChange = () => {};
 
   return (
-    <div className="color-converter" >
+    <div className="color-converter" style={{ backgroundColor: hex }}>
       <h2>Конвертер цветов</h2>
       <label>RGB:</label>
-      <input
-        type="text"
-        value={}
-        onChange={}
-        placeholder="Например: 255, 0, 0"
-      />
+      <div>
+        {rgb.map((value, index) => (
+          <input
+            key={index}
+            type="text"
+            value={value}
+            onChange={(e) => handleRgbChange(index, e.target.value)}
+            placeholder={["R", "G", "B"][index]}
+            maxLength={3}
+          />
+        ))}
+      </div>
       <label>HEX:</label>
       <input
         type="text"
         value={hex}
-        onChange={}
-        placeholder="Например: #ff0000"
+        onChange={handleHexChange}
+        placeholder="#FF0000"
+        maxLength={7}
       />
     </div>
   );
